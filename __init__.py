57abaf89ea6a72b34a6bf2ef6f56a3968d4cc086
yicheng <160642431+yichengup@users.noreply.github.com>
2024-12-10 08:24:15 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .nodes import StyleModelAdvancedApply
+
+NODE_CLASS_MAPPINGS = {
+    "StyleModelAdvancedApply": StyleModelAdvancedApply,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "StyleModelAdvancedApply": "Redux Style Advanced",
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
