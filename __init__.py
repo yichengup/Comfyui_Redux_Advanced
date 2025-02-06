b75cb88109922b08b577f207c3d6c96c159c8193
yicheng <160642431+yichengup@users.noreply.github.com>
2025-02-07 01:42:20 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from .nodes import *
+from .nodes2 import StyleAdvancedApply
 
 NODE_CLASS_MAPPINGS = {
     "YC_LG_Redux": YC_LG_Redux,
+    "StyleAdvancedApply": StyleAdvancedApply,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "YC_LG_Redux": "YC_LG_Redux_Advance",
-
+    "StyleAdvancedApply": "Style Advanced Apply",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
